3d162359a84342654b05e0b2c5af991a56576495
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-09-29 19:32:21 -0700

    add constant for both of them for type of animal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,6 +1,8 @@
 public class Animal extends Zoological {
+	public static final boolean DATABASE_TYPE = false;
+
 	public Animal (String name) {
 		this.name = name;
-		this.type = false;
+		this.type = DATABASE_TYPE;
 	}
 }
--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,6 +1,8 @@
 public class EndangeredAnimal extends Zoological {
+	public static final boolean DATABASE_TYPE = true;
+
 	public EndangeredAnimal (String name) {
 		this.name = name;
-		this.type = true;
+		this.type = DATABASE_TYPE;
 	}
 }
