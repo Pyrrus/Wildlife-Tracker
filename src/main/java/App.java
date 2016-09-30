99452c7ca38e8d7f68636a3894b63eaf53de20fd
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-09-30 14:28:05 -0700

    Made three route for the site for viewing animals and home page.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,10 +15,31 @@ public class App {
 
     get("/", (request, response) -> {
       Map <String, Object> model = new HashMap <String, Object>();
-
+      model.put("template", "templates/index.vtl");
+      model.put("title", "Portland Wildlife Tracker");
+      model.put("header", header);
+      model.put("css", "");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    
+    get("/animal", (request, response) -> {
+      Map <String, Object> model = new HashMap <String, Object>();
+      model.put("template", "templates/list-animal.vtl");
+      model.put("animal", Animal.all());
+      model.put("title", "Animal list");
+      model.put("header", header);
+      model.put("css", "");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/endangeredanimal", (request, response) -> {
+      Map <String, Object> model = new HashMap <String, Object>();
+      model.put("template", "templates/list-enanimal.vtl");
+      model.put("title", "Endangered Animal List");
+      model.put("animal", EndangeredAnimal.all());
+      model.put("header", header);
+      model.put("css", "");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
